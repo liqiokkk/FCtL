28d3d9f9fc7371456762555d292b766be34c97d2
liqi <37665079+liqiokkk@users.noreply.github.com>
2021-08-19 15:58:52 +0800

    Update option.py

--- option.py
+++ option.py
@@ -35,7 +35,7 @@ class Options():
 
     def parse(self):
         args = self.parser.parse_args()
-        args.num_epochs = 50
+        args.num_epochs = 100
         args.start = 0
         args.lens = 50
         args.lr = 5e-5
