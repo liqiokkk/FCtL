e8d7774114a51bf7f5aaf55c568551de9842a47c
liqi520 <541261403@qq.com>
2021-08-20 11:17:01 +0800

    Create train_pre.sh

---
+++ train_pre.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0
+python train_deep_globe.py \
+--task_name "Bfcn" \
+--mode 1 \
+--dataset 2 \
+--batch_size 6 \
+--sub_batch_size 6 \
+--size_p 508 \
+--size_g 508 \
+--train \
+--val 
