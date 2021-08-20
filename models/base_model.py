dcb096ff007c0ba652dd502ac62f9c2280b60275
liqi520 <541261403@qq.com>
2021-08-20 11:16:12 +0800

    Create base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+import logging
+import torch.nn as nn
+import numpy as np
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def forward(self):
+        raise NotImplementedError
+
+    def summary(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        nbr_params = sum([np.prod(p.size()) for p in model_parameters])
+
+    def __str__(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        nbr_params = sum([np.prod(p.size()) for p in model_parameters])
+        return super(BaseModel, self).__str__()
+        #return summary(self, input_shape=(2, 3, 224, 224))
