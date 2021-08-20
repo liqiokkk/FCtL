3fdd64795ed4ff1256e012e3e963f3bde0611072
liqi520 <541261403@qq.com>
2021-08-20 11:16:48 +0800

    Create train_B10.sh

---
+++ train_B10.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=0
+python train_deep_globe.py \
+--task_name "B10" \
+--mode 2 \
+--dataset 2 \
+--batch_size 6 \
+--sub_batch_size 4 \
+--size_p 508 \
+--size_g 508 \
+--pre_path Bfcn.epoch.pth \
+--train \
+--val 
