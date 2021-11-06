3a1523efb7cca95dd8cb8abb5f4d52be675a8263
liqi <37665079+liqiokkk@users.noreply.github.com>
2021-11-06 15:56:33 +0800

    Update lr_scheduler.py

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -1,13 +1,3 @@
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Created by: Hang Zhang
-## ECE Department, Rutgers University
-## Email: zhang.hang@rutgers.edu
-## Copyright (c) 2017
-##
-## This source code is licensed under the MIT-style license found in the
-## LICENSE file in the root directory of this source tree
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 import math
 
 class LR_Scheduler(object):
